a3211f0bd984c36c41aba78f15fd45b84bd8ee8f
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-03-04 20:57:08 +0400

    Реклама с картинкой

--- server.py
+++ server.py
@@ -8,10 +8,11 @@ host = "127.0.0.1"
 
 @app.route("/")
 def slash():
-    return '</br>'.join(["Миссия Колонизация Марса",
+    return '</br>'.join(["<h1>Миссия Колонизация Марса</h1>",
                          f'<a href="/promotion">Рекламная кампания</a>',
                          f'<a href="/slogan">Девиз</a>',
-                         f'<a href="/image_mars">Изображение Марса</a>'])
+                         f'<a href="/image_mars">Изображение Марса</a>',
+                         f'<a href="/promotion_image">Реклама с картинкой</a>'])
 
 
 @app.route("/promotion")
@@ -26,13 +27,33 @@ def promotion():
 @app.route("/image_mars")
 def image_mars():
     return '</br>'.join(["<h1>Жди нас, Марс!</h1>",
-                         f'''<img src="{url_for('static', filename='img/image_mars.jpg')}"''',
+                         f'''<img src="{url_for('static', filename='img/image_mars.jpg')}">''',
                          "Жди нас, Марс!"])
 
 
 @app.route("/slogan")
 def slogan():
     return "И на Марсе будут яблони цвести!"
+
+
+@app.route("/promotion_image")
+def promotion_image():
+    return f'''<!doctype html>
+                <html lang="ru">
+                <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" href="{url_for("static", filename="css/style.css")}">
+                </head>
+                <body>
+                    <h1>Жди нас, марс!</h1>
+                    <img src="{url_for('static', filename='img/image_mars.jpg')}">
+                    <p class="p1"><br>Человечество вырастает из детства.</p>
+                    <p class="p2"><br>Человечеству мала одна планета.</p>
+                    <p class="p3"><br>Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                    <p class="p4"><br>И начнем с Марса!</p>
+                    <p class="p5"><br>Присоединяйся!</p>
+                </body>
+                </html>'''
 
 
 if __name__ == '__main__':
